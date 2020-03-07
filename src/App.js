079277280d38e99d8d78e38e494a5059c1b4445f
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-03-07 01:37:52 -0300

    Visualizador de Border-Radius

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { TextField as MUITextField } from "@material-ui/core";
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-top: 100px;
+`;
+
+const ContainerInputs = styled.div`
+  margin: 40px;
+`;
+
+const TextField = styled(MUITextField)`
+  margin: 8px;
+`;
+
+const Title = styled.h3`
+  font-size: 40px;
+  text-align: center;
+  text-transform: uppercase;
+  color: #424874;
+  font-family: Arial, Helvetica, sans-serif;
+  margin-bottom: 25px;
+`;
 
 function App() {
+  const [topLeft, setTopLeft] = useState(0);
+  const [topRight, setTopRight] = useState(0);
+  const [bottomRight, setBottomRight] = useState(0);
+  const [bottomLeft, setBottomLeft] = useState(0);
+
+  const styles = {
+    box: {
+      borderRadius: `${topLeft}px ${topRight}px ${bottomRight}px ${bottomLeft}px`,
+      backgroundColor: "#000",
+      height: 250,
+      width: 250
+    },
+    inputs: {
+      margin: 8
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Title>Border Radius Previewer</Title>
+      <Container>
+        <div style={styles.box}></div>
+
+        <ContainerInputs>
+          <TextField
+            type="number"
+            label="TOP LEFT"
+            style={styles.inputs}
+            value={topLeft}
+            onChange={e => {
+              setTopLeft(e.target.value);
+            }}
+          />
+          <TextField
+            type="number"
+            label="TOP RIGHT"
+            style={styles.inputs}
+            value={topRight}
+            onChange={e => {
+              setTopRight(e.target.value);
+            }}
+          />
+          <TextField
+            type="number"
+            label="BOTTOM RIGHT"
+            value={bottomRight}
+            style={styles.inputs}
+            onChange={e => {
+              setBottomRight(e.target.value);
+            }}
+          />
+          <TextField
+            type="number"
+            label="BOTTOM LEFT"
+            value={bottomLeft}
+            style={styles.inputs}
+            onChange={e => {
+              setBottomLeft(e.target.value);
+            }}
+          />
+        </ContainerInputs>
+      </Container>
+    </>
   );
 }
 
